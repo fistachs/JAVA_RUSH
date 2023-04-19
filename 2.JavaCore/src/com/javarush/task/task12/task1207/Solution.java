5ad4247cbeb7112aa9a55277ab7ec6aac145dd7f
enderme19@gmail.com <enderme19@gmail.com>
2023-04-19 12:19:08 +0300

    19.04(commit)

---
+++ 2.JavaCore/src/com/javarush/task/task12/task1207/Solution.java
@@ -0,0 +1,20 @@
+package com.javarush.task.task12.task1207;
+
+/* 
+Int и Integer
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        print(10);
+        Integer sss = 100;
+        print(sss);
+    }
+
+    public static void print(int x ){
+
+    }
+    public static void print(Integer s){
+
+    }
+}
---
+++ 2.JavaCore/src/com/javarush/task/task12/task1208/Solution.java
@@ -0,0 +1,32 @@
+package com.javarush.task.task12.task1208;
+
+/* 
+Свобода печати
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    public static void print() {
+
+    }
+
+    public static void print(int n) {
+
+    }
+
+    public static void print(int n, int n2) {
+
+    }
+
+    public static int print(int a, long b) {
+        return 0;
+    }
+
+    public static long print(long b, int a) {
+        return 0;
+    }
+
+}
