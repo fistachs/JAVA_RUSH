c385be31ccc4bb1a15189b14d31c1d36c1bb77ca
enderme19@gmail.com <enderme19@gmail.com>
2023-04-24 11:28:54 +0300

    24.04(commit)

---
+++ 2.JavaCore/src/com/javarush/task/task14/task1420/Solution.java
@@ -0,0 +1,34 @@
+package com.javarush.task.task14.task1420;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+/* 
+НОД
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        int first = Integer.parseInt(reader.readLine());
+        int second = Integer.parseInt(reader.readLine());
+
+        System.out.println(getNod(first,second));
+    }
+    
+    private static int getNod(int first,int second){
+        if(first<1 || second <1){
+            throw new IllegalArgumentException();
+        }
+        while(first!=second){
+            if(first >second){
+                first-=second;
+            }
+            if(second>first){
+                second-=first;
+            }
+        }
+        return first;
+    }
+}
