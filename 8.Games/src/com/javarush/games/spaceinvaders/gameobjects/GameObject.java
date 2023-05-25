61195525f5fd04ede0cdddf1846037164bcb2d99
enderme19@gmail.com <enderme19@gmail.com>
2023-05-25 11:21:57 +0300

    25.05(commit)

---
+++ 8.Games/src/com/javarush/games/spaceinvaders/gameobjects/GameObject.java
@@ -0,0 +1,11 @@
+package com.javarush.games.spaceinvaders.gameobjects;
+
+public class GameObject {
+    public GameObject(double x,double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public double x;
+    public double y;
+}
