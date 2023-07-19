12cd2fb0df85ba5ec87a165c709c2a72b3e6cd46
enderme19@gmail.com <enderme19@gmail.com>
2023-07-19 20:51:24 +0300

    19.07

--- 8.Games/src/com/javarush/games/spaceinvaders/gameobjects/Boss.java
+++ 8.Games/src/com/javarush/games/spaceinvaders/gameobjects/Boss.java
@@ -1,18 +1,41 @@
 package com.javarush.games.spaceinvaders.gameobjects;
 
+import com.javarush.games.spaceinvaders.Direction;
 import com.javarush.games.spaceinvaders.ShapeMatrix;
 
-public class Boss extends EnemyShip{
+public class Boss extends EnemyShip {
     private int frameCount = 0;
+
     public Boss(double x, double y) {
         super(x, y);
         super.setAnimatedView(ShapeMatrix.BOSS_ANIMATION_FIRST, ShapeMatrix.BOSS_ANIMATION_SECOND);
     }
+
     @Override
-    public void nextFrame(){
+    public void nextFrame() {
         frameCount++;
         if (frameCount % 10 == 0 || !isAlive) {
             super.nextFrame();
         }
+    }
+
+    @Override
+    public Bullet fire() {
+        if (!isAlive) {
+            return null;
+        }
+        if (matrix == ShapeMatrix.BOSS_ANIMATION_FIRST) {
+            return new Bullet(x + 6, y + height, Direction.DOWN);
+        } else  {
+            return new Bullet(x, y + height, Direction.DOWN);
+        }
+    }
+    @Override
+    public void kill(){
+        if(!isAlive){
+            return;
+        }
+        isAlive = false;
+        super.setAnimatedView(ShapeMatrix.KILL_BOSS_ANIMATION_FIRST, ShapeMatrix.KILL_BOSS_ANIMATION_SECOND, ShapeMatrix.KILL_BOSS_ANIMATION_THIRD);
     }
 }
