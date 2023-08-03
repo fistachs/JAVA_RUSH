c6ae81a62fb4f0b4d9e839862acdeb8d8f51b81e
enderme19@gmail.com <enderme19@gmail.com>
2023-08-03 21:09:49 +0300

    3.08

--- 8.Games/src/com/javarush/games/game2048/Game2048.java
+++ 8.Games/src/com/javarush/games/game2048/Game2048.java
@@ -4,21 +4,76 @@ import com.javarush.engine.cell.*;
 
 public class Game2048 extends Game {
     private static final int SIDE = 4;
-    private int [][] gameField = new int [SIDE][SIDE];
+    private int[][] gameField = new int[SIDE][SIDE];
+
     @Override
-    public void initialize(){
-        setScreenSize(SIDE,SIDE);
+    public void initialize() {
+        setScreenSize(SIDE, SIDE);
         createGame();
         drawScene();
     }
-    private void createGame(){
 
+    private void createGame() {
+        createNewNumber();
+        createNewNumber();
     }
-    private void drawScene(){
-        for (int i = 0; i < SIDE; i++) {
-            for (int j = 0; j < SIDE; j++) {
-                setCellColor(i,j,Color.BEIGE);
+
+    private void createNewNumber() {
+        boolean isCreated = false;
+        do {
+            int x = getRandomNumber(SIDE);
+            int y = getRandomNumber(SIDE);
+            if (gameField[y][x] == 0) {
+                gameField[y][x] = getRandomNumber(10) < 9 ? 2 : 4;
+                isCreated = true;
             }
         }
+        while (!isCreated);
+    }
+
+    private void setCellColoredNumber(int x, int y, int value) {
+        Color color = getColorByValue(value);
+        String str = value > 0 ? "" + value : "";
+        setCellValueEx(x, y, color, str);
+    }
+
+    private Color getColorByValue(int value) {
+        switch (value) {
+            case 0:
+                return Color.WHITE;
+            case 2:
+                return Color.PLUM;
+            case 4:
+                return Color.SLATEBLUE;
+            case 8:
+                return Color.DODGERBLUE;
+            case 16:
+                return Color.DARKTURQUOISE;
+            case 32:
+                return Color.MEDIUMSEAGREEN;
+            case 64:
+                return Color.LIMEGREEN;
+            case 128:
+                return Color.DARKORANGE;
+            case 256:
+                return Color.SALMON;
+            case 512:
+                return Color.ORANGERED;
+            case 1024:
+                return Color.DEEPPINK;
+            case 2048:
+                return Color.MEDIUMVIOLETRED;
+            default:
+                return Color.NONE;
+        }
+    }
+
+    private void drawScene() {
+        for (int y = 0; y < SIDE; y++) {
+            for (int x = 0; x < SIDE; x++) {
+                setCellColoredNumber(x, y, gameField[y][x]);
+            }
+        }
+
     }
 }
