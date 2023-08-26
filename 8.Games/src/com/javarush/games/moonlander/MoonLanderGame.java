38817edce172ed7cba758e445dab1170cad48443
enderme19@gmail.com <enderme19@gmail.com>
2023-08-26 13:51:40 +0300

    26.08

--- 8.Games/src/com/javarush/games/moonlander/MoonLanderGame.java
+++ 8.Games/src/com/javarush/games/moonlander/MoonLanderGame.java
@@ -5,12 +5,15 @@ import com.javarush.engine.cell.Game;
 public class MoonLanderGame extends Game {
     public static final int WIDTH = 64;
     public static final int HEIGHT = 64;
+    private Rocket rocket;
     @Override
     public void initialize(){
         setScreenSize(WIDTH,HEIGHT);
         createGame();
     }
-    private void createGame(){
+
+    private void createGame() {
+        rocket = new Rocket(WIDTH / 2.0, 0);
         drawScene();
     }
     private void drawScene(){
@@ -19,5 +22,6 @@ public class MoonLanderGame extends Game {
                 setCellColor(i,j,Color.ORANGE);
             }
         }
+        rocket.draw(this);
     }
 }
--- 8.Games/src/com/javarush/games/moonlander/Rocket.java
+++ 8.Games/src/com/javarush/games/moonlander/Rocket.java
@@ -1,6 +1,6 @@
 package com.javarush.games.moonlander;
 
-public class Rocket extends GameObject{
+public class Rocket extends GameObject {
     public Rocket(double x, double y) {
         super(x, y, ShapeMatrix.ROCKET);
     }
