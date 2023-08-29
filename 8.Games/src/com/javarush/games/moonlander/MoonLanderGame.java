59bcd9fcd0e604630b2c434695e9260b66e70826
enderme19@gmail.com <enderme19@gmail.com>
2023-08-29 20:04:54 +0300

    29.08

--- 8.Games/src/com/javarush/games/moonlander/MoonLanderGame.java
+++ 8.Games/src/com/javarush/games/moonlander/MoonLanderGame.java
@@ -6,14 +6,16 @@ public class MoonLanderGame extends Game {
     public static final int WIDTH = 64;
     public static final int HEIGHT = 64;
     private Rocket rocket;
+    private GameObject landscape;
     @Override
     public void initialize(){
         setScreenSize(WIDTH,HEIGHT);
+        showGrid(false);
         createGame();
     }
 
     private void createGame() {
-        rocket = new Rocket(WIDTH / 2.0, 0);
+        createGameObjects();
         drawScene();
     }
     private void drawScene(){
@@ -23,5 +25,10 @@ public class MoonLanderGame extends Game {
             }
         }
         rocket.draw(this);
+        landscape.draw(this);
+    }
+    private void createGameObjects(){
+        rocket = new Rocket(WIDTH / 2.0, 0);
+        landscape = new GameObject(0, 25, ShapeMatrix.LANDSCAPE);
     }
 }
