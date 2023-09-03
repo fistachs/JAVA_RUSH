6c86671a518dcf07993db78fec221a67403183d4
enderme19@gmail.com <enderme19@gmail.com>
2023-09-03 12:26:54 +0300

    3.09

--- 8.Games/src/com/javarush/games/moonlander/Rocket.java
+++ 8.Games/src/com/javarush/games/moonlander/Rocket.java
@@ -3,13 +3,28 @@ package com.javarush.games.moonlander;
 public class Rocket extends GameObject {
     private double speedY = 0;
     private double boost = 0.05;
+    private double speedX = 0;
 
     public Rocket(double x, double y) {
         super(x, y, ShapeMatrix.ROCKET);
     }
 
-    public void move() {
-        speedY += boost;
+    public void move(boolean isUpPressed, boolean isLeftPressed, boolean isRightPressed) {
+        if (isUpPressed == true) {
+            speedY -= boost;
+
+        } else if (isUpPressed == false) {
+            speedY += boost;
+        }
         y += speedY;
+
+        if (isLeftPressed) {
+            speedX -= boost;
+            x += speedX;
+        } else if (isRightPressed) {
+            speedX += boost;
+            x += speedX;
+        }
+
     }
 }
