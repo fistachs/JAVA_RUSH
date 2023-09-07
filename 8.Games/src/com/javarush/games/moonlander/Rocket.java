fbb6aed17d07a0410dca5c29560881813bd974eb
enderme19@gmail.com <enderme19@gmail.com>
2023-09-07 12:05:00 +0300

    7.09

--- 8.Games/src/com/javarush/games/moonlander/Rocket.java
+++ 8.Games/src/com/javarush/games/moonlander/Rocket.java
@@ -4,29 +4,34 @@ import static com.javarush.games.moonlander.MoonLanderGame.WIDTH;
 
 public class Rocket extends GameObject {
     private double speedY = 0;
-    private double boost = 0.05;
     private double speedX = 0;
+    private double boost = 0.05;
+    private double slowdown = boost / 10;
 
     public Rocket(double x, double y) {
         super(x, y, ShapeMatrix.ROCKET);
     }
 
     public void move(boolean isUpPressed, boolean isLeftPressed, boolean isRightPressed) {
-        if (isUpPressed == true) {
+        if (isUpPressed) {
             speedY -= boost;
-
-        } else if (isUpPressed == false) {
+        } else {
             speedY += boost;
         }
         y += speedY;
 
         if (isLeftPressed) {
             speedX -= boost;
-            x += speedX;
         } else if (isRightPressed) {
             speedX += boost;
-            x += speedX;
+        } else if (speedX > slowdown) {
+            speedX -= slowdown;
+        } else if (speedX < -slowdown) {
+            speedX += slowdown;
+        } else {
+            speedX = 0;
         }
+        x += speedX;
         checkBorders();
 
     }
@@ -35,12 +40,11 @@ public class Rocket extends GameObject {
         if (x < 0) {
             x = 0;
             speedX = 0;
-        }
-        if (x + width > WIDTH) {
-            x = WIDTH - width;
+        } else if (x + width > MoonLanderGame.WIDTH) {
+            x = MoonLanderGame.WIDTH - width;
             speedX = 0;
         }
-        if (y < 0) {
+        if (y <= 0) {
             y = 0;
             speedY = 0;
         }
