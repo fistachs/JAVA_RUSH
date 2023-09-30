103304074af204887438f9442ca2e96d35f7a375
enderme19@gmail.com <enderme19@gmail.com>
2023-09-30 22:04:09 +0300

    30.09

--- 8.Games/src/com/javarush/games/moonlander/MoonLanderGame.java
+++ 8.Games/src/com/javarush/games/moonlander/MoonLanderGame.java
@@ -1,13 +1,13 @@
 package com.javarush.games.moonlander;
 
 import com.javarush.engine.cell.*;
-import com.javarush.engine.cell.Game;
 
 public class MoonLanderGame extends Game {
     public static final int WIDTH = 64;
     public static final int HEIGHT = 64;
     private Rocket rocket;
     private GameObject landscape;
+    private GameObject platform;
     private boolean isUpPressed;
     private boolean isLeftPressed;
     private boolean isRightPressed;
@@ -81,16 +81,21 @@ public class MoonLanderGame extends Game {
     private void createGameObjects() {
         rocket = new Rocket(WIDTH / 2.0, 0);
         landscape = new GameObject(0, 25, ShapeMatrix.LANDSCAPE);
-
+        platform = new GameObject(23, HEIGHT - 1, ShapeMatrix.PLATFORM);
     }
 
     private void check() {
-
+        if (rocket.isCollision(platform) && rocket.isStopped()) {
+            win();
+        } else if (rocket.isCollision(landscape)) {
+            gameOver();
+        }
     }
 
     private void win() {
 
     }
+
     private void gameOver() {
 
     }
